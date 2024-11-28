fa231a5aa8f928b033857b61a32ccb5f4d5791ea
kimjunyo <kimjunyo@github.com>
2024-11-28 22:05:37 +0900

    Feat: 아이템 좋아요 test

--- src/test/java/com/team9/sungdaehanmarket/service/ItemServiceTest.java
+++ src/test/java/com/team9/sungdaehanmarket/service/ItemServiceTest.java
@@ -14,8 +14,10 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -118,48 +120,113 @@ public class ItemServiceTest {
     @Transactional
     @DisplayName(value = "아이템 좋아요 test")
     public void likeItem(){
+        //Given
+        User seller = new User();
+        seller.setEmail("bomin@gmail.com");
+        seller.setPassword("test2");
+        seller.setName("test2");
+        seller.setUsername("bomin");
+        seller.setRating(1.0f);
+        seller.setProfileImage("예쁜사진");
+        seller.setMajor("보건학과");
 
+        userRepository.save(seller);
+
+        Item item = new Item();
+        item.setCategory(Item.Category.TEXTBOOK);
+        item.setTitle("전공 서적");
+        item.setPrice(10000L);
+        item.setSellerId(2000L);
+        item.setPhotos(List.of());
+        item.setLikes(3);
+
+        itemRepository.save(item);
+
+        Set<Long> favoriteItems = new HashSet<>();
+        Set<Long> favoriteItems2 = new HashSet<>();
+        favoriteItems.add(item.getIdx());
+
+        User buyer = new User();
+        buyer.setEmail("kimjunyo@gmail.com");
+        buyer.setPassword("test");
+        buyer.setName("test");
+        buyer.setUsername("kimjunyo");
+        buyer.setRating(1.0f);
+        buyer.setMajor("수학과");
+        buyer.setFavoriteItems(favoriteItems);
+
+        User buyer2= new User();
+        buyer2.setEmail("kimjunyo2@gmail.com");
+        buyer2.setPassword("test");
+        buyer2.setName("test");
+        buyer2.setUsername("kimjunyo2");
+        buyer2.setRating(1.0f);
+        buyer2.setMajor("수학과");
+        buyer2.setFavoriteItems(favoriteItems2);
+
+        userRepository.save(buyer);
+        userRepository.save(buyer2);
+        //When
+        boolean b = itemService.likeItem(buyer.getIdx(), item.getIdx());
+        boolean c = itemService.likeItem(buyer2.getIdx(), item.getIdx());
+        //Then
+        assertThat(b).isTrue();
+        assertThat(c).isTrue();
+        assertThat(item.getLikes()).isEqualTo(3);
+        assertThat(itemService.likeItem(2000L, item.getIdx())).isEqualTo(false);
     }
 
     @Test
     @Transactional
     @DisplayName(value = "아이템 상세정보 test")
     public void getItemDetail(){
-
+        //Given
+        //When
+        //Then
     }
 
     @Test
     @Transactional
     @DisplayName(value = "좋아요 누른 아이템 가져오기 test")
     public void getLikeItems(){
-
+        //Given
+        //When
+        //Then
     }
 
     @Test
     @Transactional
     @DisplayName(value = "판매한 아이템 가져오기 test")
     public void getSellingItems(){
-
+        //Given
+        //When
+        //Then
     }
 
     @Test
     @Transactional
     @DisplayName(value = "구매한 아이템 가져오기 test")
     public void getBuyingItems(){
-
+        //Given
+        //When
+        //Then
     }
 
     @Test
     @Transactional
     @DisplayName(value = "아이템 구매후기 가져오기 test")
     public void getItemRatingInfo(){
-
+        //Given
+        //When
+        //Then
     }
 
     @Test
     @Transactional
     @DisplayName(value = "아이템 구매후기 저장 test")
     public void saveRating(){
-
+        //Given
+        //When
+        //Then
     }
 }
